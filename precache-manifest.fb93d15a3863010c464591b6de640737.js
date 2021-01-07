c84a23d8a562f1bf90ec82a04b1957119e71d074
Timokhin Pavel <60778218+Lesmor2001@users.noreply.github.com>
2021-01-07 07:42:11 +0600

    Add files via upload

---
+++ precache-manifest.fb93d15a3863010c464591b6de640737.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a58dca70d82962fedbac3a37516e099",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9433038b7fe9bfe9577f",
+    "url": "./static/css/2.e092a416.chunk.css"
+  },
+  {
+    "revision": "85c1076fd9d197e2abf6",
+    "url": "./static/css/main.508c3e33.chunk.css"
+  },
+  {
+    "revision": "9433038b7fe9bfe9577f",
+    "url": "./static/js/2.636c7ce5.chunk.js"
+  },
+  {
+    "revision": "ee4f9df198b79cd1d557aa77b58129d3",
+    "url": "./static/js/2.636c7ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c1076fd9d197e2abf6",
+    "url": "./static/js/main.a3d08cc4.chunk.js"
+  },
+  {
+    "revision": "09b2b020936da1072150",
+    "url": "./static/js/runtime-main.8e8830eb.js"
+  },
+  {
+    "revision": "3aa3a14beee61b7e03c5e4a66261e681",
+    "url": "./static/media/calibri.3aa3a14b.woff2"
+  },
+  {
+    "revision": "5e3e7f91e642e534035b755953c8bc0d",
+    "url": "./static/media/calibri.5e3e7f91.woff"
+  },
+  {
+    "revision": "1df73acea98fbac32f627bf2cffb2033",
+    "url": "./static/media/calibrib.1df73ace.woff"
+  },
+  {
+    "revision": "b8f9d2aa3eb450e9b9f0862e3bda83ef",
+    "url": "./static/media/calibrib.b8f9d2aa.woff2"
+  },
+  {
+    "revision": "f22f6f2fcdbfda9754f861788a8610d1",
+    "url": "./static/media/index-sale.f22f6f2f.png"
+  }
+]);
